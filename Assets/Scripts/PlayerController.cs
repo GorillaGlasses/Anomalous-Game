023dff9caab9a75270f3ca5e37192b4ad5d7a6b6
GorillaGlasses <kevinlundin@comcast.net>
2026-03-07 19:12:01 -0500

    Modified the camera to follow the player and added the ability to move the camera instead of the player.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,34 +7,78 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 1.5f;
     public Rigidbody2D playerBody;
+    private Transform camPos;
+    private bool camLocked = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // In start, find the camera object and its transform, then put that transform into the camPos variable and change the position to hover over the player
     void Start()
     {
-        
+        GameObject.FindGameObjectWithTag("MainCamera").TryGetComponent<Transform>(out Transform camForm);
+        camPos = camForm;
+        camPos.position = new UnityEngine.Vector3(playerBody.position.x, playerBody.position.y, -10);
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Basic Movement Code
+        // Basic Movement Code, calls a function to make sure the next tile can be moved to.
         if(Input.GetKeyDown(KeyCode.W)){
-            checkDestinationEmpty(new UnityEngine.Vector2(playerBody.position.x, playerBody.position.y + moveSpeed));
+            if(camLocked){
+                checkDestinationEmpty(new UnityEngine.Vector2(playerBody.position.x, playerBody.position.y + moveSpeed));
+            }
+            else
+            {
+                camPos.position = new UnityEngine.Vector3(camPos.position.x, camPos.position.y + moveSpeed, -10);
+            }
         }
 
         if(Input.GetKeyDown(KeyCode.A)){
-            checkDestinationEmpty(new UnityEngine.Vector2(playerBody.position.x - moveSpeed, playerBody.position.y));
+            if(camLocked){
+                checkDestinationEmpty(new UnityEngine.Vector2(playerBody.position.x - moveSpeed, playerBody.position.y));
+            }
+            else
+            {
+                camPos.position = new UnityEngine.Vector3(camPos.position.x - moveSpeed, camPos.position.y, -10);
+            }
         }
 
         if(Input.GetKeyDown(KeyCode.S)){
-            checkDestinationEmpty(new UnityEngine.Vector2(playerBody.position.x, playerBody.position.y - moveSpeed));
+            if(camLocked){
+                checkDestinationEmpty(new UnityEngine.Vector2(playerBody.position.x, playerBody.position.y - moveSpeed));
+            }
+            else
+            {
+                camPos.position = new UnityEngine.Vector3(camPos.position.x, camPos.position.y - moveSpeed, -10);
+            }
         }
 
         if(Input.GetKeyDown(KeyCode.D)){
-            checkDestinationEmpty(new UnityEngine.Vector2(playerBody.position.x + moveSpeed, playerBody.position.y));
+            if(camLocked){
+                checkDestinationEmpty(new UnityEngine.Vector2(playerBody.position.x + moveSpeed, playerBody.position.y));
+            }
+            else
+            {
+                camPos.position = new UnityEngine.Vector3(camPos.position.x + moveSpeed, camPos.position.y, -10);
+            }
+        }
+        
+        // Enables camera movement by unlocking the camera to the player. When locked off, instead of moving the player, the camera instead moves. When locked on, 
+        // the camera is set to the player's position and follows the player.
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            if (camLocked)
+            {
+                camLocked = false;
+            } 
+            else
+            {
+                camLocked = true;
+                camPos.position = new UnityEngine.Vector3(playerBody.position.x, playerBody.position.y, -10);
+            }
         }
     }
 
-    //Checks if the spot the player is about to move to is an unoccupied tile.
+    //Checks if the spot the player is about to move to is an unoccupied tile. Moves player and camera to tile if it is open, and does nothing if not.
     void checkDestinationEmpty(UnityEngine.Vector2 direction)
     {
         UnityEngine.Vector2 targetSpot = direction; 
@@ -43,6 +87,7 @@ public class PlayerController : MonoBehaviour
         if (spotCheck.gameObject.TryGetComponent<TileVacancy>(out TileVacancy tVac) && tVac.occupant == null)
         {
             playerBody.position = targetSpot;
+            camPos.position = new UnityEngine.Vector3(targetSpot.x, targetSpot.y, -10);
         }
         return;
     }
