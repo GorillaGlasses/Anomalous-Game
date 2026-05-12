af18f4fe447cfa808e8b6b15919d2e54818a4bd2
GorillaGlasses <kevinlundin@comcast.net>
2026-05-12 00:51:02 -0400

    Added enemy pathfinding and chasing, next is adding combat

--- Assets/Scripts/TileVacancy.cs
+++ Assets/Scripts/TileVacancy.cs
@@ -7,13 +7,11 @@ public class TileVacancy : MonoBehaviour
     public GameObject occupant;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Occupied by " + collision.gameObject.tag);
         occupant = collision.gameObject;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        Debug.Log(collision.gameObject.name + " left");
         occupant = null;
     }
 }
